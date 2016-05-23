2a071d666e5ef2edd0455ae1485cd62e71b10c39
shimonben <sbalul20@gmail.com>
2016-05-23 17:41:16 +0300

    Created a method that saves the data into csv files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy
 import Steel
 import Genome
 import Population
+import csv
 
 CONST_SEQUENCE_LENGTH = 20
 CONST_POPULATION_SIZE = 50
@@ -16,33 +17,47 @@ CONST_MIN_THICKNESS = 0.1
 CONST_MIN_WIDTH = 0.1
 CONST_MIN_ZINC_THICKNESS = 0.1
 CONST_MIN_STEEL_GRADE = 0.1
-CONST_GENERATIONS = 50
+CONST_GENERATIONS = 500
 
 
+# TODO dont forget to change the weights in Steel calss
 def main():
-    lst = initialize_the_steel_coils()
-    dict = lst[0]
-    coils = lst[1]
-    population = Population.Population(coils)
-    population.createInitial(CONST_POPULATION_SIZE)
-    for i in range(CONST_GENERATIONS):
-        population.updateGenesRange()
-        selected = population.rouletteSelection()
-        offspring = selected[0].crossover(selected[1])
-        c1 = offspring[0]
-        c2 = offspring[1]
-        c1.mutate(CONST_MUTATION_PROBABILITY)
-        c2.mutate(CONST_MUTATION_PROBABILITY)
-        c1.evaluate(c1.getSequence(), Steel.calculate_max_penalty(), population.coils)
-        c2.evaluate(c2.getSequence(), Steel.calculate_max_penalty(), population.coils)
-        #population.replacement_both_parents(selected[0], selected[1], offspring[0], offspring[1])
-        #population.replacement_random(offspring[0], offspring[1])
-        population.replacement_elitism(offspring[0], offspring[1])
-        population.update_fitness()
-        best = population.get_best_solution()
-        total_fit = population.getFitness()
-        temp = "Generation: {:0>-3}, Total fit: {:07.4f}, {:0>-2} is the best Fit: {:07.4f}".format(i, total_fit, best[0], best[1])
-        print(temp)
+    with open('NM.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for t in range(100):
+            lst = initialize_the_steel_coils()
+            dict = lst[0]
+            coils = lst[1]
+            population = Population.Population(coils)
+            population.createInitial(CONST_POPULATION_SIZE)
+            for i in range(CONST_GENERATIONS):
+                if i == 0:
+                    total_fit = population.getFitness()
+                    best = population.get_best_solution()
+                    temp = [total_fit, best[1]]
+                population.updateGenesRange()
+                selected = population.rouletteSelection()
+                offspring = selected[0].crossover(selected[1])
+                c1 = offspring[0]
+                c2 = offspring[1]
+                c1.mutate(CONST_MUTATION_PROBABILITY)
+                c2.mutate(CONST_MUTATION_PROBABILITY)
+                c1.evaluate(c1.getSequence(), Steel.calculate_max_penalty(), population.coils)
+                c2.evaluate(c2.getSequence(), Steel.calculate_max_penalty(), population.coils)
+                # population.replacement_both_parents(selected[0], selected[1], offspring[0], offspring[1])
+                population.replacement_random(offspring[0], offspring[1])
+                # population.replacement_elitism(offspring[0], offspring[1])
+                population.update_fitness()
+                best = population.get_best_solution()
+                total_fit = population.getFitness()
+                # temp = "Generation: {:0>-3}, Total fit: {:07.4f}, {:0>-2} is the best Fit: {:07.4f}".format(i, total_fit, best[0], best[1])
+                # print(temp)
+            total_fit = population.getFitness()
+            best = population.get_best_solution()
+            total_dif = total_fit - temp[0]
+            best_dif = best[1] - temp [1]
+            spamwriter.writerow(['%s' % total_dif, '%s' % best_dif])
 
 
 def initialize_the_steel_coils():
