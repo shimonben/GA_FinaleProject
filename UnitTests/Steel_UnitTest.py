93dc74029cd8e38bfdb52bda2a9710751bece0ce
shimonben <sbalul20@gmail.com>
2016-10-23 10:44:06 +0300

    fixed steel unit

--- UnitTests/Steel_UnitTest.py
+++ UnitTests/Steel_UnitTest.py
@@ -1,10 +1,24 @@
 import unittest
-import Population
+import Steel
 import GAutils
+import Population
 
 
 class TestSteelClass(unittest.TestCase):
-    def (self):
+    def test_calculate_max_penalty(self):
+        # we calculated the max penalty outside the program: 29.18
+        self.assertEqual(29.18, Steel.calculate_max_penalty())
+
+    def test_calculate_transition_penalty(self):
+        # we calculated the transition penalty between the 2 first coils outside the program: 0.1885
+        coils = GAutils.get_coils_from_excel()
+        population = Population.Population(coils)
+        population.createInitial(GAutils.CONST_POPULATION_SIZE)
+        # coils[0] is pf type Steel and holds the attributes for the coils
+        temp = coils[0].calculate_penalty(coils[1])/Steel.calculate_max_penalty()
+        temp = float("%.4f" % temp)
+        self.assertAlmostEqual(temp, 0.1885)
+
 
 if __name__ == '__main__':
     unittest.main()
